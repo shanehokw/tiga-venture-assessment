b06d20db8dab06e3e58b12382e03521e8a3c4415
shanehokw <kenwae@hotmail.com>
2024-11-24 20:35:29 +0800

    add custom attribute status to task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,4 +12,19 @@ class Task extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded = ['id'];
+
+    protected $appends = ['status'];
+
+    protected function getStatusAttribute(): string{
+        $dueDate = Carbon::parse($this->due_date); // Parse the due_date as Carbon instance
+        $today = Carbon::now();
+
+        if ($dueDate->isPast()) {
+            return 'Overdue';
+        } elseif ($dueDate->diffInDays($today) <= 7) {
+            return 'Due soon';
+        } else {
+            return 'Not urgent';
+        }
+    }
 }
--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Link } from '@inertiajs/vue3';
 
 defineProps({ tasks: Object });
 </script>
@@ -7,8 +8,20 @@ defineProps({ tasks: Object });
 <template>
     <Head title="Tasks" />
     <AuthenticatedLayout>
-        <div v-for="task in tasks" :key="task.id">
-            {{ task.name }}
+        <div class="mt-3 flex justify-center">
+            <div class="w-3/5 rounded-2xl bg-white p-4">
+                <div class="flex flex-wrap gap-y-4 rounded-xl">
+                    <h2 class="text-4xl">Your Tasks</h2>
+                    <Link
+                        v-for="task in tasks"
+                        :key="task.id"
+                        :href="`/task/${task.id}/edit`"
+                        class="w-full rounded-xl"
+                    >
+                        {{ task.name }} - {{ task.status }}
+                    </Link>
+                </div>
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
